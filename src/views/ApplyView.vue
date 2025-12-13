2bdbf0e29732cb76a45d3a2b6ba54296c218563e
Apelng <administrator@apelasset.com>
2025-12-13 20:33:03 +0100

    date responsiveness on mobile

--- src/views/ApplyView.vue
+++ src/views/ApplyView.vue
@@ -125,7 +125,7 @@
         </div>
 
         <div class="form-row form-row--2col">
-          <div class="form-group" style="width: 80%">
+          <div class="form-group" style="width: 80%; overflow-x: hidden;">
             <label class="form-label">Date of Birth <span class="required">*</span></label>
             <input v-model="formData.dob" type="date" class="form-input form-input--date" />
           </div>
